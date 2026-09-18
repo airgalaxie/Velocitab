8c3edb6a26133e4a840914c12a135647f61319b4
jens <git@airbrush-galaxie.de>
2026-09-18 19:24:28 +0200

    Fix team packets for Minecraft 26.3

--- src/main/java/net/william278/velocitab/velocityinternal/packet/Protocol770Adapter.java
+++ src/main/java/net/william278/velocitab/velocityinternal/packet/Protocol770Adapter.java
@@ -44,7 +44,8 @@ public class Protocol770Adapter extends Protocol765Adapter {
                 ProtocolVersion.MINECRAFT_1_21_9,
                 ProtocolVersion.MINECRAFT_1_21_11,
                 ProtocolVersion.MINECRAFT_26_1,
-                ProtocolVersion.MINECRAFT_26_2
+                ProtocolVersion.MINECRAFT_26_2,
+                ProtocolVersion.MINECRAFT_26_3
         ));
     }
 
